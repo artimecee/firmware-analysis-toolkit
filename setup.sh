ed4dea0a3638f0d4e3d0dbfc5d247adb3e0275cc
artimecee <96160489+artimecee@users.noreply.github.com>
2024-06-06 22:27:43 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ then
 fi
 
 echo "Installing binwalk"
-git clone --depth=1 https://github.com/OSPG/binwalk.git
+git clone https://github.com/liyansong2018/binwalk.git
 cd binwalk
 
 # Temporary fix for sasquatch failing to install (From https://github.com/ReFirmLabs/binwalk/pull/601)
